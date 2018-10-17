7c0133adb77224f8037f9893f04cc7b3fb38e5a8
wugui <970730659@qq.com>
2018-10-17 21:15:20 +0800

    iRuntime

---
+++ GiantServer/GiantNode/Interface/IRunTime.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GiantNode.Interface
+{
+    public interface IRunTime
+    {
+        uint GroupId { get; }
+
+        uint NodeId { get; }
+
+        string NodeName { get; }
+
+        uint[] Nodes { get; }
+
+        bool IsFrontNode { get; }
+
+        Dictionary<string, string> Param { get; }
+
+        string GetParam(string key);
+    }
+}
