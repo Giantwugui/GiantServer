cb8ccf41d255d1ccb1f7b656bf1c5c158c16bf2d
wugui <970730659@qq.com>
2018-10-17 21:21:41 +0800

    1

---
+++ GiantServer/GiantCore/LogOut.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GiantCore
+{
+    public class Log
+    {
+        public static void LogOut(LogType logType, string content)
+        {
+        }
+    }
+}
