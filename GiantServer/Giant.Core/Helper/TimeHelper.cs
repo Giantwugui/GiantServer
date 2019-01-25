9415c176980d95f429b5e6efcf299b35c732d06e
wugui <970730659@qq.com>
2019-01-25 14:18:17 +0800

    删除无用文件

---
+++ GiantServer/Giant.Core/Helper/TimeHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Giant.Core
+{
+	public static class TimeHelper
+	{
+
+		public static long Now()
+		{
+			return (DateTime.UtcNow.Ticks - epoch) / 10000;
+		}
+
+		public static long NowSeconds()
+		{
+			return (DateTime.UtcNow.Ticks - epoch) / 10000000;
+		}
+
+		private static readonly long epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).Ticks;
+    }
+}
