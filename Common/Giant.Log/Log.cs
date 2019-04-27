f14ba9f4b79663a3769fd292628c2152038121bb
wugui <970730659@qq.com>
2019-04-27 18:33:29 +0800

    NLog

---
+++ Common/Giant.Log/Log.cs
@@ -0,0 +1,60 @@
+﻿using NLog;
+using System;
+
+namespace Giant.Log
+{
+    public class Logger
+    {
+		private readonly NLog.Logger logger = LogManager.GetLogger("Logger");
+
+        public void Debug(string msg, params object[] args)
+        {
+            logger.Debug(msg, args);
+        }
+
+        public void Debug(string msg, Exception err)
+        {
+            logger.Debug(msg, err);
+        }
+
+        public void Info(string msg, params object[] args)
+        {
+            logger.Info(msg, args);
+        }
+
+        public void Info(string msg, Exception err)
+        {
+            logger.Info(msg, err);
+        }
+
+        public void Trace(string msg, params object[] args)
+        {
+            logger.Trace(msg, args);
+        }
+
+        public void Trace(string msg, Exception err)
+        {
+            logger.Trace(msg, err);
+        }
+
+        public void Error(string msg, params object[] args)
+        {
+            logger.Error(msg, args);
+        }
+
+        public void Error(string msg, Exception err)
+        {
+            logger.Error(msg, err);
+        }
+
+        public void Fatal(string msg, params object[] args)
+        {
+            logger.Fatal(msg, args);
+        }
+
+        public void Fatal(string msg, Exception err)
+        {
+            logger.Fatal(msg, err);
+        }
+    }
+}
