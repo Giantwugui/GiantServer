327d9a21b050c3c8356c86a9aff0337c505a5605
wugui <970730659@qq.com>
2019-06-13 21:49:00 +0800

    grap 例子

---
+++ Examples/Grpc/Grpc.Test.Client/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Grpc.Core;
+using MathMethod;
+using Math = MathMethod.Math;
+
+namespace Grpc.Test.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var channel = new Channel("127.0.0.1", 23456, ChannelCredentials.Insecure);
+
+            Math.MathClient client = new Math.MathClient(channel);
+            MathExamples.DivExample(client);
+
+            MathExamples.DivAsyncExample(client).Wait();
+
+            MathExamples.FibExample(client).Wait();
+
+            MathExamples.SumExample(client).Wait();
+
+            MathExamples.DivManyExample(client).Wait();
+
+            MathExamples.DependendRequestsExample(client).Wait();
+
+            channel.ShutdownAsync().Wait();
+        }
+    }
+}
