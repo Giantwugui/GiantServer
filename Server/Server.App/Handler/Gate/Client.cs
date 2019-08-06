a83352781592bd0489daf4017d947285bcfc7222
wugui <970730659@qq.com>
2019-08-06 16:56:53 +0800

    遗漏文件

---
+++ Server/Server.App/Handler/Gate/Client.cs
@@ -0,0 +1,31 @@
+﻿using Giant.Net;
+using Giant.Share;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server.App
+{
+    public class Client
+    {
+        private readonly Session session;
+
+        private AESCrypt AESCrypt;
+        public string SecretKey => AESCrypt.EncryptKey;
+
+        public string Account { get; private set; }
+        public int Uid { get; private set; }
+
+        public Client(string account, int uid, Session session)
+        {
+            Account = account;
+            Uid = uid;
+            this.session = session;
+        }
+
+        public void BuildSecretKey()
+        {
+            AESCrypt = new AESCrypt();
+        }
+    }
+}
---
+++ Server/Server.App/Handler/Gate/ClientManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Giant.Net;
+using Giant.Share;
+
+namespace Server.App
+{
+    public class ClientManager
+    {
+        private Dictionary<int, Client> clients = new Dictionary<int, Client>();
+        public static ClientManager Instance { get; } = new ClientManager();
+
+        public void Add(Client client)
+        {
+            clients.Add(client.Uid, client);
+        }
+
+        public void Remove(int uid)
+        {
+            clients.Remove(client.Uid);
+        }
+    }
+}
