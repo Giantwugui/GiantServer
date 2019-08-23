ee143f4dd5fef29733d523016d92fa239217b8a1
wugui <970730659@qq.com>
2019-08-23 16:48:51 +0800

    添加账号登录逻辑

---
+++ Server/Server.Account/Handler/ClientHandle_Login.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Giant.Msg;
+using Giant.Net;
+using Giant.Share;
+using Giant.Log;
+using Giant.DB.MongoDB;
+
+namespace Server.Account
+{
+    [MessageHandler(AppType.Account)]
+    class ClientHandle_Login : RpcMHandler<Msg_CA_Login, Msg_AC_Login>
+    {
+        public override async Task Run(Session session, Msg_CA_Login request, Msg_AC_Login response)
+        {
+            var query = new MongoDBQuery<AccountInfo>("Account", x => x.Account == request.Account);
+            AccountInfo account = await query.Task();
+            if (account == null)
+            {
+                account = new AccountInfo() { Account = request.Account, Password = request.Password };
+                var insertQuery = new MongoDBInsert<AccountInfo>("Account", account);
+                await insertQuery.Task();
+            }
+
+            //负载均衡一个gate
+            response.Address = "127.0.0.1";
+            response.Error = ErrorCode.Success;
+
+            Logger.Debug($"user login {request.Account}");
+        }
+    }
+}
---
+++ Server/Server.Account/Model/Account.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Server.Account
+{
+    [BsonIgnoreExtraElements]
+    public class AccountInfo
+    {
+        [BsonId]
+        public string Account { get; set; }
+
+        public string Password { get; set; }
+    }
+}
