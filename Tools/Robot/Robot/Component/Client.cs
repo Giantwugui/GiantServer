4c9618a69fa358738ff2cdeefa805b2ec30098bb
wugui <970730659@qq.com>
2019-12-07 10:48:46 +0800

    robot 遗漏文件

---
+++ Tools/Robot/Robot/Component/Client.cs
@@ -0,0 +1,177 @@
+﻿using Giant.Core;
+using Giant.Msg;
+using Giant.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Robot
+{
+    public class Client : InitSystem<string>
+    {
+        private int token;
+        private Session session;
+        private AccountInfo accountInfo;
+        private string accountHost = "127.0.0.1:6002";
+        private List<int> loginedServers = new List<int>();
+        private Dictionary<int, Msg_CharacterInfo> characterInfos = new Dictionary<int, Msg_CharacterInfo>();
+
+        public string Account { get; private set; }
+        public bool IsEnterWorlded { get; private set; }
+
+
+        public override void Init(string account)
+        {
+            Account = account;
+        }
+
+        private void SetCharacters(IEnumerable<Msg_CharacterInfo> characters)
+        {
+            characterInfos.Clear();
+            foreach (var kv in characters)
+            {
+                characterInfos.Add(kv.Uid, kv);
+            }
+        }
+
+        private void SetLoginedServers(IEnumerable<int> servers)
+        {
+            loginedServers.Clear();
+            loginedServers.AddRange(servers);
+        }
+
+        public void DoLogin()
+        {
+            accountInfo = new AccountInfo(Account, "");
+            session = Scene.Pool.GetComponent<OutterNetworkComponent>().Create(accountHost);
+            session.OnConnectCallback += (aimSession, state) =>
+            {
+                if (state)
+                {
+                    Login();
+                }
+                else
+                {
+                    Console.WriteLine($"Session {aimSession.Id} disconnected from {aimSession.RemoteIPEndPoint.ToString()}");
+                    session.Dispose();
+                }
+            };
+
+            session.Start();
+        }
+
+        private async void Login()
+        {
+            Msg_CA_Login msg = new Msg_CA_Login()
+            {
+                Account = accountInfo.Account,
+                Password = accountInfo.Passward,
+            };
+
+            var result = await session.Call(msg);
+            Msg_AC_Login logResult = result as Msg_AC_Login;
+            if (result.IsSuccess())
+            {
+                Console.WriteLine($"user login success {accountInfo.Account}, logined servers {string.Join("|", logResult.Servers)}");
+
+                SetLoginedServers(logResult.Servers);
+                GetGate();
+            }
+        }
+
+        private async void GetGate()
+        {
+            Msg_CA_LoginZone msg = new Msg_CA_LoginZone()
+            {
+                Account = accountInfo.Account,
+                Zone = 1001,
+            };
+
+            Msg_AC_LoginZone result = (await session.Call(msg)) as Msg_AC_LoginZone;
+            if (result.Error == ErrorCode.Success)
+            {
+                Console.WriteLine($"Client login to gate address {result.Address}");
+
+                token = result.Token;
+                ConnectToGate(result.Address);
+            }
+        }
+
+        private void ConnectToGate(string address)
+        {
+            session?.Dispose();
+            session = Scene.Pool.GetComponent<OutterNetworkComponent>().Create(address);
+            session.OnConnectCallback += (aimSession, state) =>
+            {
+                if (state)
+                {
+                    LoginZone();
+                    session.OnConnectCallback += (session, state) =>
+                    {
+                        var player = PlayerManagerComponent.Instance.GetPlayer(session.InstanceId);
+                        player?.OnDisconnected();
+                    };
+                }
+                else
+                {
+                    Console.WriteLine($"Session {aimSession.Id} disconnected from gate {aimSession.RemoteIPEndPoint.ToString()}");
+                    session.Dispose();
+                }
+            };
+            session.Start();
+        }
+
+        private async void LoginZone()
+        {
+            Msg_CG_Login msg = new Msg_CG_Login() { Account = accountInfo.Account, Token = token };
+            Msg_GC_Login result = (await session.Call(msg)) as Msg_GC_Login;
+            if (result.IsSuccess())
+            {
+                GetCharacter();
+            }
+        }
+
+        private async void GetCharacter()
+        {
+            Msg_CG_GetCharacter msg = new Msg_CG_GetCharacter();
+            Msg_GC_GetCharacter result = (await session.Call(msg) as Msg_GC_GetCharacter);
+            SetCharacters(result.Characters);
+
+            if (result.IsSuccess())
+            {
+                if (result.Characters.Count > 0)
+                {
+                    EnterWorld(result.Characters.First().Uid);
+                }
+                else
+                {
+                    CreateCharacter(1);
+                }
+            }
+        }
+
+        private async void CreateCharacter(int roleId)
+        {
+            Msg_CG_CreateCharacter msg = new Msg_CG_CreateCharacter { RoleId = roleId };
+            Msg_GC_CreateCharacter result = (await session.Call(msg)) as Msg_GC_CreateCharacter;
+            if (result.IsSuccess())
+            {
+                EnterWorld(result.Character.Uid);
+            }
+        }
+
+        private void EnterWorld(int uid)
+        {
+            IsEnterWorlded = true;
+            Player player = ComponentFactory.CreateComponent<Player, string, int, Session>(accountInfo.Account, uid, session);
+            PlayerManagerComponent.Instance.AddPlayer(player);
+            player.EnterWorld();
+
+            int count = PlayerManagerComponent.Instance.PlayerCount();
+            if (count % 20 == 0)
+            {
+                Console.WriteLine($"total count {ClientManagerComponent.Instance.TotalCount} login {count} cost {ClientManagerComponent.Instance.Stopwatch.ElapsedMilliseconds}");
+            }
+        }
+    }
+}
---
+++ Tools/Robot/Robot/Component/ClientManagerComponent.cs
@@ -0,0 +1,37 @@
+﻿using Giant.Core;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Robot
+{
+    public class ClientManagerComponent : InitSystem
+    {
+        private static string accountPre = "wuu";
+        private Queue<Client> wattingLoginList = new Queue<Client>();
+
+        public int TotalCount { get; set; }
+        public Stopwatch Stopwatch { get; private set; }
+        public static ClientManagerComponent Instance { get; private set; }
+        public override void Init()
+        {
+            Instance = this;
+        }
+
+        public void CreateClients(int count)
+        {
+            TotalCount = count;
+            for (int i = 0; i < count; ++i)
+            {
+                wattingLoginList.Enqueue(ComponentFactory.CreateComponentWithParent<Client, string>(this, accountPre + i));
+            }
+
+            Stopwatch = new Stopwatch();
+            Stopwatch.Start();
+
+            while (wattingLoginList.TryDequeue(out var player))
+            {
+                player.DoLogin();
+            }
+        }
+    }
+}
