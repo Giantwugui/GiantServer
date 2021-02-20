d4979fb1793b3e50bd62b147baed7c8599b77ecc
wugui <wuugui@qq.com>
2021-02-20 18:22:21 +0800

    添加加密相关内容

---
+++ Tools/Robot/Robot/Net/RsaTest.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Robot
+{
+    public class RsaTest
+    {
+        public static void Test()
+        {
+            string publicKey, privateKey;
+            string path = Directory.GetCurrentDirectory();
+            string publicKeyPath = Path.Combine(path, "PublicKey.txt");
+            string privateKeyPath = Path.Combine(path, "PrivateKey.txt");
+
+            if (!File.Exists(publicKeyPath) || !File.Exists(privateKeyPath))
+            {
+                RSACryptoServiceProvider SACryptoServiceProvider = new RSACryptoServiceProvider(1024);
+                publicKey = SACryptoServiceProvider.ToXmlString(false);
+                privateKey = SACryptoServiceProvider.ToXmlString(true);
+                SACryptoServiceProvider.Dispose();
+
+                File.WriteAllText(publicKeyPath, publicKey);
+                File.WriteAllText(privateKeyPath, privateKey);
+            }
+            else
+            {
+                publicKey = File.ReadAllText(publicKeyPath);
+                privateKey = File.ReadAllText(privateKeyPath);
+            }
+
+            string testStr = "hello world";
+            string encrypt = RSAEncrypt(privateKey, testStr);
+            string decode = RSADecrypt(privateKey, encrypt);
+        }
+
+        /// <summary>
+        /// RSA的加密函数
+        /// </summary>
+        /// <param name="xmlPublicKey">公钥</param>
+        /// <param name="encryptString">待加密的字符串</param>
+        /// <returns></returns>
+        public static string RSAEncrypt(string xmlPublicKey, string encryptString)
+        {
+            try
+            {
+                byte[] PlainTextBArray;
+                byte[] CypherTextBArray;
+                string Result;
+                System.Security.Cryptography.RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+                rsa.FromXmlString(xmlPublicKey);
+                PlainTextBArray = (new UnicodeEncoding()).GetBytes(encryptString);
+                CypherTextBArray = rsa.Encrypt(PlainTextBArray, false);
+                Result = Convert.ToBase64String(CypherTextBArray);
+                return Result;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        /// <summary>
+        /// RSA的加密函数 
+        /// </summary>
+        /// <param name="xmlPublicKey">公钥</param>
+        /// <param name="EncryptString">待加密的字节数组</param>
+        /// <returns></returns>
+        public string RSAEncrypt(string xmlPublicKey, byte[] EncryptString)
+        {
+            try
+            {
+                byte[] CypherTextBArray;
+                string Result;
+                System.Security.Cryptography.RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+                rsa.FromXmlString(xmlPublicKey);
+                CypherTextBArray = rsa.Encrypt(EncryptString, false);
+                Result = Convert.ToBase64String(CypherTextBArray);
+                return Result;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// RSA的解密函数
+        /// </summary>
+        /// <param name="xmlPrivateKey">私钥</param>
+        /// <param name="decryptString">待解密的字符串</param>
+        /// <returns></returns>
+        public static string RSADecrypt(string xmlPrivateKey, string decryptString)
+        {
+            try
+            {
+                byte[] PlainTextBArray;
+                byte[] DypherTextBArray;
+                string Result;
+                System.Security.Cryptography.RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+                rsa.FromXmlString(xmlPrivateKey);
+                PlainTextBArray = Convert.FromBase64String(decryptString);
+                DypherTextBArray = rsa.Decrypt(PlainTextBArray, false);
+                Result = (new UnicodeEncoding()).GetString(DypherTextBArray);
+                return Result;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        /// <summary>
+        /// RSA的解密函数 
+        /// </summary>
+        /// <param name="xmlPrivateKey">私钥</param>
+        /// <param name="DecryptString">待解密的字节数组</param>
+        /// <returns></returns>
+        public string RSADecrypt(string xmlPrivateKey, byte[] DecryptString)
+        {
+            try
+            {
+                byte[] DypherTextBArray;
+                string Result;
+                System.Security.Cryptography.RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+                rsa.FromXmlString(xmlPrivateKey);
+                DypherTextBArray = rsa.Decrypt(DecryptString, false);
+                Result = (new UnicodeEncoding()).GetString(DypherTextBArray);
+                return Result;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+    }
+}
