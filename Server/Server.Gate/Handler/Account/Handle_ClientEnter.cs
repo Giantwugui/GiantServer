3cf63dfa1e4446692587a6b2bd093edddd9c1fda
wugui <wuugui@qq.com>
2021-02-22 10:21:22 +0800

    调整信息处理结构

---
+++ Server/Server.Gate/Handler/Account/Handle_ClientEnter.cs
@@ -0,0 +1,17 @@
+﻿using Giant.Core;
+using Giant.Msg;
+using Giant.Net;
+using System.Threading.Tasks;
+
+namespace Server.Gate
+{
+    [MessageHandler]
+    public class Handle_ClientEnter : MHandler<Msg_AG_ClientLogin>
+    {
+        public override Task Run(Session session, Msg_AG_ClientLogin message)
+        {
+            ClientManagerComponent.Instance.WillEnter(new ClientEnter(message.Account, message.Token));
+            return Task.CompletedTask;
+        }
+    }
+}
