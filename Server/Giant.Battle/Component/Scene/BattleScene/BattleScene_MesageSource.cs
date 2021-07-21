d4fadc9529eea553bdec0de00daa33a6147e2f0e
wugui <wuugui@qq.com>
2021-07-21 18:24:52 +0800

    结构调整

---
+++ Server/Giant.Battle/Component/Scene/BattleScene/BattleScene_MesageSource.cs
@@ -0,0 +1,28 @@
+﻿using Giant.Model;
+using Giant.Util;
+
+namespace Giant.Battle
+{
+    /*
+     * 统一构造IMessage，用于通知前端
+     */
+    partial class BattleScene : IBattleSceneEvent
+    {
+        #region 广播
+
+        public void OnBattleStart()
+        {
+        }
+
+        public void OnBattleStop(MapModel model, BattleResult result)
+        {
+            PlayerList.ForEach(x => x.Value.MsgListener.OnBattleStop(model, result));
+        }
+
+        public void OnBattleEnd()
+        {
+        }
+
+        #endregion
+    }
+}
